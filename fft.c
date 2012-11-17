6d702617dd34b7db48b2aa217ccfabe706432dd6
Darren Kulp <darren@kulp.ch>
2012-11-17 02:24:51 -0600

    Refactoring

--- fft.c
+++ fft.c
@@ -22,6 +22,79 @@ static const double freqs[] = {
     2025., 2225.,
 };
 
+int process_bit(size_t bit_base, fftw_complex *data, fftw_complex *fft_result, int *channel, int *bit)
+{
+    size_t maxi = -1;
+    {
+        double max = -1;
+        for (int i = 0; i < SIZE; i++) {
+            //printf("fft_result[%d] = { %2.2f, %2.2f }\n", i, fft_result[i][0], fft_result[i][1]);
+            double mag = sqrt(pow(fft_result[i][0],2) + pow(fft_result[i][1],2));
+            #if VERBOSE
+            printf("magnitude[%d] = { %6.2f }\n", i, mag);
+            #endif
+            if (mag > max && (lowend / PERBIN) <= i && i <= (highend / PERBIN)) {
+                max = mag;
+                maxi = i;
+            }
+        }
+    }
+
+    #if VERBOSE
+    printf("bucket with greatest magnitude was %zd, which corresponds to frequency range [%4.0f, %4.0f)\n",
+            maxi, PERBIN * maxi, PERBIN * (maxi + 1));
+    #endif
+
+    double min = DBL_MAX;
+    size_t mini = -1;
+    double freq = maxi * PERBIN + (PERBIN / 2.);
+    #if VERBOSE
+    printf("midpoint frequency is %4.0f\n", freq);
+    #endif
+    for (int i = 0; i < countof(freqs); i++) {
+        double t = fabs(freq - freqs[i]);
+        if (t < min) {
+            min = t;
+            mini = i;
+        }
+    }
+
+    *channel = mini / 2 + 1;
+    *bit = mini % 2;
+
+    return 0;
+}
+
+int process_byte(size_t size, double input[size])
+{
+    fftw_complex *data       = fftw_malloc(SIZE * sizeof *data);
+    fftw_complex *fft_result = fftw_malloc(SIZE * sizeof *fft_result);
+
+    for (int bit = 0; bit < size / SAMPLES_PER_BIT; bit++) {
+        fftw_plan plan_forward  = fftw_plan_dft_1d(SIZE, data, fft_result, FFTW_FORWARD, FFTW_ESTIMATE);
+
+        size_t bit_base = bit * SAMPLES_PER_BIT;
+
+        for (int i = 0; i < SIZE; i++) {
+            data[i][0] = i < SAMPLES_PER_BIT ? input[bit_base + i] : 0.;
+            data[i][1] = 0.;
+        }
+
+        fftw_execute(plan_forward);
+
+        int channel, bit;
+        process_bit(bit_base, data, fft_result, &channel, &bit);
+        printf("Guess : channel %zd bit %zd\n", channel, bit);
+
+        fftw_destroy_plan(plan_forward);
+    }
+
+    fftw_free(data);
+    fftw_free(fft_result);
+
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
     if (argc < 2)
@@ -42,62 +115,7 @@ int main(int argc, char** argv)
     printf("baud rate is %4d\n", BAUD_RATE);
     printf("samples per bit is %4.0f\n", SAMPLES_PER_BIT);
 
-    for (int bit = 0; bit < index / SAMPLES_PER_BIT; bit++) {
-        size_t bit_base = bit * SAMPLES_PER_BIT;
-
-        fftw_complex *data       = fftw_malloc(SIZE * sizeof *data);
-        fftw_complex *fft_result = fftw_malloc(SIZE * sizeof *fft_result);
-
-        fftw_plan plan_forward  = fftw_plan_dft_1d(SIZE, data, fft_result, FFTW_FORWARD, FFTW_ESTIMATE);
-
-        for (int i = 0; i < SIZE; i++) {
-            data[i][0] = i < SAMPLES_PER_BIT ? input[bit_base + i] : 0.;
-            data[i][1] = 0.;
-        }
-
-        fftw_execute(plan_forward);
-
-        size_t maxi = -1;
-        {
-            double max = -1;
-            for (int i = 0; i < SIZE; i++) {
-                //printf("fft_result[%d] = { %2.2f, %2.2f }\n", i, fft_result[i][0], fft_result[i][1]);
-                double mag = sqrt(pow(fft_result[i][0],2) + pow(fft_result[i][1],2));
-                #if VERBOSE
-                printf("magnitude[%d] = { %6.2f }\n", i, mag);
-                #endif
-                if (mag > max && (lowend / PERBIN) <= i && i <= (highend / PERBIN)) {
-                    max = mag;
-                    maxi = i;
-                }
-            }
-        }
-
-        #if VERBOSE
-        printf("bucket with greatest magnitude was %zd, which corresponds to frequency range [%4.0f, %4.0f)\n",
-                maxi, PERBIN * maxi, PERBIN * (maxi + 1));
-        #endif
-
-        double min = DBL_MAX;
-        size_t mini = -1;
-        double freq = maxi * PERBIN + (PERBIN / 2.);
-        #if VERBOSE
-        printf("midpoint frequency is %4.0f\n", freq);
-        #endif
-        for (int i = 0; i < countof(freqs); i++) {
-            double t = fabs(freq - freqs[i]);
-            if (t < min) {
-                min = t;
-                mini = i;
-            }
-        }
-
-        printf("Guess : channel %zd bit %zd\n", mini / 2 + 1, mini % 2);
-
-        fftw_destroy_plan(plan_forward);
-        fftw_free(data);
-        fftw_free(fft_result);
-    }
+    process_byte(index, input);
 
     return 0;
 }
