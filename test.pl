960f5308f76c3b8318c4eff03a39e5ed3b0729b6
Darren Kulp <darren@kulp.ch>
2012-12-08 20:25:27 -0600

    Simple tester

---
+++ test.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+
+my $trials = 1000;
+my $maxlen = 10;
+my $fname = "trial.wav";
+
+for my $trial (1 .. $trials) {
+    print "Trial $trial of $trials ...\n";
+
+    my $length = rand $maxlen;
+    my @bytes = map { int rand 256 } 1 .. $length;
+    system("./gen -o $fname @bytes");
+    my @lines = (qx(./fft $fname 2> /dev/null))[0 .. scalar $#bytes];
+    chomp, s/^.*= //, $_ = hex $_ for @lines;
+}
+
+sub check
+{
+    for my $i (0 .. $#{ $_[0] }) {
+        die unless $_[0][$i] == $_[1][$i];
+    }
+}
+
