c90d1a4681b0400e03bedd622de58b29e82d0b24
Darren Kulp <darren@kulp.ch>
2012-12-09 18:31:59 -0600

    Default to 8khz sample rate

--- test.pl
+++ test.pl
@@ -4,14 +4,15 @@ use strict;
 my $trials = 1000;
 my $maxlen = 10;
 my $fname = "trial.wav";
+my $rate = 8000;
 
 for my $trial (1 .. $trials) {
     print "Trial $trial of $trials ...\n";
 
     my $length = rand $maxlen;
     my @bytes = map { int rand 256 } 1 .. $length;
-    system("./gen -o $fname @bytes");
-    my @lines = (qx(./fft $fname 2> /dev/null))[0 .. scalar $#bytes];
+    system("./gen -s $rate -o $fname @bytes");
+    my @lines = (qx(./fft -s $rate $fname 2> /dev/null))[0 .. scalar $#bytes];
     chomp, s/^.*= //, $_ = hex $_ for @lines;
 }
 
