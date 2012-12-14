6a0caa69aaad3e2f19d14716d01abf175c3e8232
Darren Kulp <darren@kulp.ch>
2012-12-13 22:10:52 -0600

    Actually do checking in test.pl
    Embarrassingly, the check() function was not being called.
    
    Check longer strings, since accumulated offsets don't show up right
    away.
    
    Default gain to 0.5.

--- test.pl
+++ test.pl
@@ -2,18 +2,20 @@
 use strict;
 
 my $trials = 1000;
-my $maxlen = 10;
+my $maxlen = 100;
 my $fname = "trial.wav";
 my $rate = 8000;
+my $genopts = "-G .5";
 
 for my $trial (1 .. $trials) {
     print "Trial $trial of $trials ...\n";
 
     my $length = rand $maxlen;
     my @bytes = map { int rand 256 } 1 .. $length;
-    system("./gen -s $rate -o $fname @bytes");
+    system("./gen -s $rate -o $fname $genopts @bytes");
     my @lines = (qx(./fft -s $rate $fname 2> /dev/null))[0 .. scalar $#bytes];
     chomp, s/^.*= //, $_ = hex $_ for @lines;
+    check(\@lines, \@bytes);
 }
 
 sub check
