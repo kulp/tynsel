fa020672541c9e0c5533908b167e846a83001472
Darren Kulp <darren@kulp.ch>
2019-12-20 11:51:01 -0700

    Switch to uint16_t samples

--- wrap.c
+++ wrap.c
@@ -6,7 +6,7 @@
 const int SAMPLE_RATE = 8000;
 const int FREQUENCY = 1270;
 
-float get_sample(uint8_t *phase, uint8_t step, const float sines[64])
+uint16_t get_sample(uint8_t *phase, uint8_t step, const uint16_t sines[64])
 {
     bool half    = *phase & (1 << 7);
     bool quarter = *phase & (1 << 6);
@@ -14,10 +14,10 @@ float get_sample(uint8_t *phase, uint8_t step, const float sines[64])
 
     *phase += step;
 
-    return sines[lookup] * (half ? -1 : 1);
+    return sines[lookup] ^ -half;
 }
 
-void run(int samples, float output[samples], const float sines[64])
+void run(int samples, uint16_t output[samples], const uint16_t sines[64])
 {
     uint8_t phase = 0;
     const uint8_t step = SAMPLE_RATE / FREQUENCY;
@@ -29,19 +29,19 @@ void run(int samples, float output[samples], const float sines[64])
 
 int main()
 {
-    float sines[64];
+    uint16_t sines[64];
 
     for (int i = 0; i < 64; i++) {
-        sines[i] = sinf(2 * M_PI * (i + 0.5) / 256);
+        sines[i] = sinf(2 * M_PI * (i + 0.5) / 256) * INT16_MAX + INT16_MIN;
     }
 
     const int samples = 64 * 8;
-    float output[samples];
+    uint16_t output[samples];
 
     run(samples, output, sines);
 
     for (int i = 0; i < samples; i++) {
-        printf("samples[%3d] = % f\n", i, output[i]);
+        printf("samples[%3d] = % f\n", i, 2.0f * (float)(output[i] - INT16_MAX) / UINT16_MAX);
     }
 }
 
