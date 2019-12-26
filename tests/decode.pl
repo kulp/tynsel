ad4a508c766c136ea5301f0ca4cd122c35c64c86
Darren Kulp <darren@kulp.ch>
2019-12-26 12:31:45 -0800

    Implement initial decoding script

---
+++ tests/decode.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+
+my $offset = shift
+    // die "Supply a offset (in samples) from the start bit edge as the first argument";
+
+my $bitwidth = 27; # approximately (8000Hz)/(300 baud)
+
+my $last;
+my $edge = -$bitwidth;
+my $bit = 0;
+my $byte = 0;
+while (<>) {
+    chomp;
+    next unless defined $last;
+    my $here = int;
+    if ($bit == 0 && $here >= 0 && $last < 0) {
+        if ($. - $edge < $bitwidth) {
+            die "found an edge at line $., sooner than expected (last edge was $edge)";
+        }
+        # found a positive-going edge
+        $edge = $.;
+    }
+
+    if ($edge + $offset + $bit * $bitwidth == $.) {
+        # sample here
+        my $found = $here >= 0 ? 0 : 1;
+
+        if ($bit == 0) {
+            # start bit, skip
+        } elsif ($bit == 8) {
+            # parity, skip
+        } elsif ($bit > 8) {
+            if ($found != 1) {
+                die "Stop bit was not 1";
+            }
+        } else {
+            $byte |= ($found << ($bit - 1));
+        }
+
+        say "found $found";
+
+        if ($bit >= 10) {
+            printf "Byte : 0b%07b (`%s`)\n", $byte, chr $byte;
+            $byte = 0;
+            $bit = 0;
+        } else {
+            $bit++;
+        }
+    }
+} continue {
+    $last = int;
+}
+
