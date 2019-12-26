93f1e271cab033c6aaddee0e5ad4fbb2739c0be1
Darren Kulp <darren@kulp.ch>
2019-12-26 09:12:10 -0800

    Add some testing scripts

---
+++ tests/rms.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+
+use List::Util qw(sum);
+
+# First argument is window size, remainder are input files
+my $n = shift;
+
+sub square ($) { $_[0] * $_[0] }
+
+my @list;
+while (<>) {
+    push @list, int;
+    say sqrt sum map square($_), $list[-$n..-1] if $n <= scalar @list;
+}
+
