ea43cfbaae2b5da85fb446214069e2a319e7c849
Darren Kulp <darren@kulp.ch>
2019-12-27 13:01:52 -0800

    Allow configuring decoder

--- tests/instance.sh
+++ tests/instance.sh
@@ -4,6 +4,8 @@ here=$(dirname $0)
 zeros_file=$here/z
 ones_file=$here/o
 
+DECODE=${DECODE:-$here/decode.pl}
+
 rms_samples=${1:-10}
 hysteresis=${2:-7}
 offset=${3:-15}
@@ -12,5 +14,5 @@ ignore_samples=${4:-5070}
 $here/compare.pl <($here/rms.pl $rms_samples $zeros_file) <($here/rms.pl $rms_samples $ones_file) |
     $here/runs.pl $hysteresis |
     tail -n+$ignore_samples |
-    $here/decode.pl $offset
+    $DECODE $offset
 
