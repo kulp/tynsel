266b8e09da9bd2fa6f0b67ee7f278d196f2ac391
Darren Kulp <darren@kulp.ch>
2019-12-28 07:51:38 -0800

    Set up hook for RMS

--- tests/instance.sh
+++ tests/instance.sh
@@ -5,13 +5,14 @@ zeros_file=$here/z
 ones_file=$here/o
 
 DECODE=${DECODE:-$here/decode.pl}
+RMS=${RMS:-$here/rms.pl}
 
 rms_samples=${1:-10}
 hysteresis=${2:-7}
 offset=${3:-15}
 ignore_samples=${4:-5070}
 
-$here/compare.pl <($here/rms.pl $rms_samples $zeros_file) <($here/rms.pl $rms_samples $ones_file) |
+$here/compare.pl <($RMS $rms_samples < $zeros_file) <($RMS $rms_samples < $ones_file) |
     $here/runs.pl $hysteresis |
     tail -n+$ignore_samples |
     $DECODE $offset
