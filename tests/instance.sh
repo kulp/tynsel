6cf1d4104a73603b599ca496080e910b4ff88764
Darren Kulp <darren@kulp.ch>
2019-12-28 12:14:34 -0800

    Set up hook for RUNS

--- tests/instance.sh
+++ tests/instance.sh
@@ -6,13 +6,14 @@ ones_file=$here/o
 
 DECODE=${DECODE:-$here/decode.pl}
 RMS=${RMS:-$here/rms.pl}
+RUNS=${RUNS:-$here/runs.pl}
 
 rms_samples=${1:-10}
 hysteresis=${2:-7}
 offset=${3:-15}
 ignore_samples=${4:-5070}
 
-$here/runs.pl $hysteresis <($RMS $rms_samples < $zeros_file) <($RMS $rms_samples < $ones_file) |
+$RUNS $hysteresis <($RMS $rms_samples < $zeros_file) <($RMS $rms_samples < $ones_file) |
     tail -n+$ignore_samples |
     $DECODE $offset
 
