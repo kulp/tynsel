efaa38eeeb4ddd869f4b4570521f374b0cc30f60
Darren Kulp <darren@kulp.ch>
2019-12-30 13:25:22 -0800

    Allow specifying filter binary

--- tests/instance.sh
+++ tests/instance.sh
@@ -15,6 +15,16 @@ ignore_samples=${4:-5070}
 
 source $here/bash_functions
 
+if [[ -n ${FILTER:-} ]]
+then
+    function filter ()
+    {
+        local file=$1
+        shift
+        sox $file -t raw -e signed -r 8000 -b 16 - | unpack | $FILTER "$@" | pack
+    }
+fi
+
 $RUNS $hysteresis <(pass_freq 1070 $input_file -b 16 -t raw - | unpack | $RMS $rms_samples) <(pass_freq 1270 $input_file -b 16 -t raw - | unpack | $RMS $rms_samples) |
     tail -n+$ignore_samples |
     $DECODE $offset
