a33f2eba7bb5673062605c9d5dd9c8f90f5a4e94
Darren Kulp <darren@kulp.ch>
2019-12-29 19:31:26 -0800

    Do text conversion within instance.sh

--- tests/instance.sh
+++ tests/instance.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 here=$(dirname $0)
-zeros_file=$here/z
-ones_file=$here/o
+zeros_file=$here/zeros.raw
+ones_file=$here/ones.raw
 
 DECODE=${DECODE:-$here/decode.pl}
 RMS=${RMS:-$here/rms.pl}
@@ -13,7 +13,12 @@ hysteresis=${2:-7}
 offset=${3:-15}
 ignore_samples=${4:-5070}
 
-$RUNS $hysteresis <($RMS $rms_samples < $zeros_file) <($RMS $rms_samples < $ones_file) |
+function textify ()
+{
+    perl -le 'local $/; print for map { unpack "s" } map { /\G(..)/gms } <>' "$@"
+}
+
+$RUNS $hysteresis <(textify $zeros_file | $RMS $rms_samples) <(textify $ones_file | $RMS $rms_samples) |
     tail -n+$ignore_samples |
     $DECODE $offset
 
