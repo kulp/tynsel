46eecc2ae1fd4a37b886df5fdda863a418932267
Darren Kulp <darren@kulp.ch>
2019-12-30 06:00:11 -0800

    Hoist functions to a common file

--- tests/instance.sh
+++ tests/instance.sh
@@ -13,31 +13,9 @@ hysteresis=${2:-7}
 offset=${3:-15}
 ignore_samples=${4:-5070}
 
-function textify ()
-{
-    perl -le 'local $/; print for map { unpack "s" } map { /\G(..)/gms } <>' "$@"
-}
+source $here/bash_functions
 
-function notch ()
-{
-    local notch_freq=${1?"Supply a notch frequency"}
-    local nyquist=$(( ${2:-8000} / 2 ))
-    local notch_width=${3:-100}
-
-    octave-cli --eval "pkg load signal; [b,a]=pei_tseng_notch($notch_freq/$nyquist,$notch_width/$nyquist); disp([b a])"
-}
-
-function invert () { sox "$@" vol -1; }
-
-function pass_freq ()
-{
-    local freq=$1
-    shift
-    local input=$1
-    sox -m <(sox $input -p biquad $(notch $freq) | invert -p -p) "$@"
-}
-
-$RUNS $hysteresis <(pass_freq 1070 $input_file -b 16 -t raw - | textify | $RMS $rms_samples) <(pass_freq 1270 $input_file -b 16 -t raw - | textify | $RMS $rms_samples) |
+$RUNS $hysteresis <(pass_freq 1070 $input_file -b 16 -t raw - | unpack | $RMS $rms_samples) <(pass_freq 1270 $input_file -b 16 -t raw - | unpack | $RMS $rms_samples) |
     tail -n+$ignore_samples |
     $DECODE $offset
 
