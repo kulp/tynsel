1d67006c4330c80b19100100995f39153491bf39
Darren Kulp <darren@kulp.ch>
2019-12-31 04:59:13 -0700

    Switch bench to new C top, and sweep notch width

--- tests/bench.sh
+++ tests/bench.sh
@@ -3,21 +3,26 @@ set -euo pipefail
 here=$(dirname $0)
 input_file=$1
 
-for rms_samples in {9..11}
+source $here/bash_functions
+
+for rms_samples in {4..25}
 do
-    for hysteresis in {6..8}
+    for hysteresis in {3..10}
     do
-        for offset in {14..16}
+        for offset in {3..20}
         do
-            (
-                out=out/$rms_samples,$hysteresis,$offset
-                if [[ ! -e $out ]] # assume existence implies previous completion
-                then
-                    $here/instance.sh $input_file $rms_samples $hysteresis $offset 2> /dev/null > $out
-                fi
-            ) &
+            for bw in 20 50 100
+            do
+                (
+                    out=$here/out/$rms_samples,$hysteresis,$offset,$bw
+                    if [[ ! -e $out ]] # assume existence implies previous completion
+                    then
+                        unpack $input_file | $here/../top $rms_samples $hysteresis $offset $(notch 1070 8000 $bw) $(notch 1270 8000 $bw) 2> /dev/null > $out
+                    fi
+                ) &
+            done
+            wait
         done
-        wait
     done
 done
 
