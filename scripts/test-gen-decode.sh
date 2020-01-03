cd71a792219c20b35a41c029dc331ab07e159bc6
Darren Kulp <darren@kulp.ch>
2020-01-03 14:19:19 -0700

    Generate random strings for testing

--- scripts/test-gen-decode.sh
+++ scripts/test-gen-decode.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -euo pipefail
-# The length of the test string is chosen to work around (in the older,
-# pre-integer implementation) a discontinuity between encoding bytes and
-# encoding the trailing carrier, a discontinuity which can look like a start
-# bit
-string="hello there, world !!"
 temp=$(mktemp -d)
 ${TRAP:-trap} "rm -rf $temp" EXIT
-echo $string > $temp/str
+LC_ALL=C tr -dc '[[:alnum:] [:punct:]\n]' < /dev/urandom | cut -b1-50 | head -n1 > $temp/str || true
 make -Bj && ./gen $(perl -e 'print join " ", map unpack("c"), map { split // } <>' $temp/str) |
     tee $temp/gen-raw |
     ./listen 0 7 10 10 12 |
