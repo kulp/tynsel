d696cab452bcc9af213588755fd43861b982d665
Darren Kulp <darren@kulp.ch>
2020-01-03 14:31:22 -0700

    Update bench.sh for new command-line rules
    Set default notch bandwidth to 150Hz

--- tests/bench.sh
+++ tests/bench.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 here=$(dirname $0)
 input_file=$1
+channel=0
 
 source $here/bash_functions
 
@@ -11,18 +12,15 @@ do
     do
         for offset in {0..20}
         do
-            for bw in 50 100 150 200
-            do
-                (
-                    out=$here/out/$rms_samples,$hysteresis,$offset,$bw
-                    if [[ ! -e $out ]] # assume existence implies previous completion
-                    then
-                        $here/../decode $rms_samples $hysteresis $offset $(notch 1070 8000 $bw) $(notch 1270 8000 $bw) < $input_file 2> /dev/null > $out
-                    fi
-                ) &
-            done
-            wait
+            (
+                out=$here/out/$rms_samples,$hysteresis,$offset
+                if [[ ! -e $out ]] # assume existence implies previous completion
+                then
+                    $here/../decode $channel $rms_samples $hysteresis $offset < $input_file 2> /dev/null > $out
+                fi
+            ) &
         done
+        wait
     done
 done
 
