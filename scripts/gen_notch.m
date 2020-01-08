723abebf671ea8d835852e5df8ee17ef984b7a86
Darren Kulp <darren@kulp.ch>
2020-01-07 17:21:39 -0700

    Check expected filter invariants

--- scripts/gen_notch.m
+++ scripts/gen_notch.m
@@ -12,6 +12,10 @@ notch_width = str2num(arg_list{3});
 
 pkg load signal;
 [b, a] = pei_tseng_notch(notch_freq / nyquist, notch_width / nyquist);
+if a(1) != 1 || b(1) != b(3) || a(2) != b(2)
+    printf('Filter invariant check failed\n')
+    exit(1)
+endif
 printf('// pei_tseng_notch(%d/(%d/2),%d/(%d/2))\n', notch_freq, sample_rate, notch_width, sample_rate)
 printf('{\n')
 format = '    .%c = { DEFINE_COEFF(%+6.6f), DEFINE_COEFF(%+6.6f), DEFINE_COEFF(%+6.6f) },\n';
