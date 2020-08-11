a190d4bd13b0edb1a39a86f750a0380bb6c198a1
Darren Kulp <darren@kulp.ch>
2020-08-11 14:52:07 -0500

    Commit default coefficients
    It should not be required to install Octave and its `signal` and
    `control` packages merely to compute nine numbers.

---
+++ coeffs_8000_150_.h
@@ -0,0 +1,24 @@
+// pei_tseng_notch(1070/(8000/2),150/(8000/2))
+{
+    .coeff_b0 = DEFINE_COEFF(+0.942846),
+    .coeff_b1 = DEFINE_COEFF(-1.258101),
+    .coeff_a2 = DEFINE_COEFF(+0.885692),
+},
+// pei_tseng_notch(1270/(8000/2),150/(8000/2))
+{
+    .coeff_b0 = DEFINE_COEFF(+0.943270),
+    .coeff_b1 = DEFINE_COEFF(-1.023338),
+    .coeff_a2 = DEFINE_COEFF(+0.886541),
+},
+// pei_tseng_notch(2025/(8000/2),150/(8000/2))
+{
+    .coeff_b0 = DEFINE_COEFF(+0.944342),
+    .coeff_b1 = DEFINE_COEFF(+0.037082),
+    .coeff_a2 = DEFINE_COEFF(+0.888683),
+},
+// pei_tseng_notch(2225/(8000/2),150/(8000/2))
+{
+    .coeff_b0 = DEFINE_COEFF(+0.944585),
+    .coeff_b1 = DEFINE_COEFF(+0.332109),
+    .coeff_a2 = DEFINE_COEFF(+0.889171),
+},
