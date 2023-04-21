28c8ce00ac0bb36ef897d30d24df7387ada6f6d8
Darren Kulp <darren@kulp.ch>
2023-04-20 21:31:44 -0400

    Use a new mechanism for generating random data

--- scripts/test-gen-decode.sh
+++ scripts/test-gen-decode.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 temp=$(mktemp -d)
 here="$(dirname "$0")"
 ${TRAP:-trap} "rm -rf $temp" EXIT
-LC_ALL=C tr -dc '[[:alnum:] [:punct:]\n]' < /dev/urandom | cut -b1-50 | head -n1 > $temp/str || true
+head -c500 /dev/urandom | LC_ALL=C tr -c '[:graph:]' ' ' > $temp/str || true
 $here/../gen -F $temp/str |
     tee $temp/gen-raw |
     $here/../listen -C 0 -W 7 -T 10 -H 10 -O 12 |
