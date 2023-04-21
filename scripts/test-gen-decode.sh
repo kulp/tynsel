2ce59c63f4bc5c6b4993c741b2c4993cf30c64aa
Darren Kulp <darren@kulp.ch>
2023-04-20 21:25:42 -0400

    Use relative paths in tester

--- scripts/test-gen-decode.sh
+++ scripts/test-gen-decode.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 temp=$(mktemp -d)
+here="$(dirname "$0")"
 ${TRAP:-trap} "rm -rf $temp" EXIT
 LC_ALL=C tr -dc '[[:alnum:] [:punct:]\n]' < /dev/urandom | cut -b1-50 | head -n1 > $temp/str || true
-./gen -F $temp/str |
+$here/../gen -F $temp/str |
     tee $temp/gen-raw |
-    ./listen -C 0 -W 7 -T 10 -H 10 -O 12 |
+    $here/../listen -C 0 -W 7 -T 10 -H 10 -O 12 |
     tee $temp/decoded |
     cmp $temp/str /dev/stdin &&
     echo good || (echo bad: $temp ; false)
