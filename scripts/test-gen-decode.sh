d7f62c5dee39eeca0592b4729047f7446847f753
Darren Kulp <darren@kulp.ch>
2023-04-20 21:24:46 -0400

    Give `cmp` two arguments always
    Some systems have one-argument `cmp`, but not all.

--- scripts/test-gen-decode.sh
+++ scripts/test-gen-decode.sh
@@ -7,5 +7,5 @@ make --environment-overrides -Bj && ./gen -F $temp/str |
     tee $temp/gen-raw |
     ./listen -C 0 -W 7 -T 10 -H 10 -O 12 |
     tee $temp/decoded |
-    cmp $temp/str &&
+    cmp $temp/str /dev/stdin &&
     echo good || (echo bad: $temp ; false)
